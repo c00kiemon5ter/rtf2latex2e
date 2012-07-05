9f6a3da4604079422700941fe11d2ba19b463126
whennings <whennings@ec8729b6-1b83-4082-b6c8-10560a4245e1>
2012-07-05 11:49:40 +0000

    updated emf2pdf.bat to allow blanks in folder names
    git-svn-id: svn://svn.code.sf.net/p/rtf2latex2e/code@273 ec8729b6-1b83-4082-b6c8-10560a4245e1

--- emf2pdf.bat
+++ emf2pdf.bat
@@ -1,10 +1,10 @@
 @echo off
-"%ProgramFiles%\Metafile to EPS Converter\metafile2eps.exe" %1 %~dpn1.eps
+"%ProgramFiles%\Metafile to EPS Converter\metafile2eps.exe" "%1" "%~dpn1.eps"
 if errorlevel 1 exit /b 9
 which epstopdf > NUL
 if errorlevel 1 exit /b 1
 rem epstopdf comes with a TeX installation, e.g. MikTeX
-call epstopdf %~dpn1.eps --outfile=%~dpn1.pdf
+call epstopdf "%~dpn1.eps" --outfile="%~dpn1.pdf"
 if errorlevel 1 exit /b 1
-rem del %~dpn1.eps
+rem del "%~dpn1.eps"
 exit /b 0
